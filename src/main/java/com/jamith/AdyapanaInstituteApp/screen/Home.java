709b952e20a3cb1c0c8e508c49f072933bc424e8
Rashmi Rajapaksha <110500099+RashSVR@users.noreply.github.com>
2024-09-25 03:29:51 +0530

    home page changed

--- src/main/java/com/jamith/AdyapanaInstituteApp/screen/Home.java
+++ src/main/java/com/jamith/AdyapanaInstituteApp/screen/Home.java
@@ -8,27 +8,23 @@ package com.jamith.AdyapanaInstituteApp.screen;
 import javax.swing.*;
 
 /**
+ *
  * @author Dell
  */
 public class Home extends javax.swing.JPanel {
-    JFrame mainFrame;
+   JFrame mainFrame;
     JPanel studentPanel = new Student();
     JPanel teacherPanel = new Teacher();
     JPanel subjectPanel = new Subject();
-    JPanel classPanel = new Class();
+    JPanel classPanel = new com.jamith.AdyapanaInstituteApp.screen.Class();
     JPanel paymentPanel = new Payment();
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton classBTN;
-    private javax.swing.JButton paymentBTN;
-    private javax.swing.JButton studentBTN;
-    private javax.swing.JButton subjectBTN;
-    private javax.swing.JButton teacherBTN;
+
 
     /**
      * Creates new form Home
      */
     public Home(JFrame mainFrame) {
-        this.mainFrame = mainFrame;
+         this.mainFrame = mainFrame;
         initComponents();
     }
 
@@ -41,6 +37,7 @@ public class Home extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
         studentBTN = new javax.swing.JButton();
         teacherBTN = new javax.swing.JButton();
         subjectBTN = new javax.swing.JButton();
@@ -49,6 +46,10 @@ public class Home extends javax.swing.JPanel {
 
         setMaximumSize(new java.awt.Dimension(32767, 688));
         setPreferredSize(new java.awt.Dimension(1125, 688));
+
+        jLabel1.setFont(new java.awt.Font("Arial", 0, 36)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Adyapana Institute");
 
         studentBTN.setText("Student Manage");
         studentBTN.addActionListener(new java.awt.event.ActionListener() {
@@ -88,33 +89,46 @@ public class Home extends javax.swing.JPanel {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addGap(79, 79, 79)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(studentBTN, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(teacherBTN, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(subjectBTN, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(classBTN, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(paymentBTN, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                .addContainerGap(893, Short.MAX_VALUE))
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(358, 358, 358)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 384, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(449, 449, 449)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(teacherBTN, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(studentBTN, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(subjectBTN, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(classBTN, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(paymentBTN, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addGap(0, 383, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addGap(210, 210, 210)
-                                .addComponent(studentBTN)
-                                .addGap(27, 27, 27)
-                                .addComponent(teacherBTN)
-                                .addGap(27, 27, 27)
-                                .addComponent(subjectBTN)
-                                .addGap(27, 27, 27)
-                                .addComponent(classBTN)
-                                .addGap(28, 28, 28)
-                                .addComponent(paymentBTN)
-                                .addContainerGap(224, Short.MAX_VALUE))
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(110, 110, 110)
+                .addComponent(jLabel1)
+                .addGap(99, 99, 99)
+                .addComponent(studentBTN)
+                .addGap(27, 27, 27)
+                .addComponent(teacherBTN)
+                .addGap(27, 27, 27)
+                .addComponent(subjectBTN)
+                .addGap(27, 27, 27)
+                .addComponent(classBTN)
+                .addGap(28, 28, 28)
+                .addComponent(paymentBTN)
+                .addContainerGap(218, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void studentBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_studentBTNActionPerformed
+        mainFrame.setContentPane(studentPanel);
+        mainFrame.setVisible(true);
+    }//GEN-LAST:event_studentBTNActionPerformed
 
     private void teacherBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_teacherBTNActionPerformed
         mainFrame.setContentPane(teacherPanel);
@@ -136,9 +150,13 @@ public class Home extends javax.swing.JPanel {
         mainFrame.setVisible(true);
     }//GEN-LAST:event_paymentBTNActionPerformed
 
-    private void studentBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_studentBTNActionPerformed
-        mainFrame.setContentPane(studentPanel);
-        mainFrame.setVisible(true);
-    }//GEN-LAST:event_studentBTNActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton classBTN;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton paymentBTN;
+    private javax.swing.JButton studentBTN;
+    private javax.swing.JButton subjectBTN;
+    private javax.swing.JButton teacherBTN;
     // End of variables declaration//GEN-END:variables
 }
